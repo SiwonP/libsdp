67b7d10a1798c67067a7e207961f58f28f09a42b
Simon Petit <nomisp96@hotmail.fr>
2018-05-02 20:37:36 +0200

    MINOR changing names

--- src/description.h
+++ src/description.h
@@ -33,16 +33,16 @@ struct lsdp_origin_t {
 
 
 /**
- * @typedef lsdp_time_description_t
+ * @typedef lsdp_time_t
  *
  */
-typedef struct lsdp_time_description_t lsdp_time_description_t;
+typedef struct lsdp_time_t lsdp_time_t;
 
 /**
- * @struct lsdp_time_description_t
+ * @struct lsdp_time_t
  *
  */
-struct lsdp_time_description_t {
+struct lsdp_time_t {
     char *start; /**< Start time of the session. */
     char *stop; /**< Stop time of the session. */
     char *repeat_interval; /**< .*/
@@ -81,16 +81,16 @@ struct lsdp_encryption_key_t {
 };
 
 /**
- * @typedef lsdp_connection_description_t
+ * @typedef lsdp_connection_t
  *
  */
-typedef struct lsdp_connection_description_t lsdp_connection_description_t;
+typedef struct lsdp_connection_t lsdp_connection_t;
 
 /**
- * @struct lsdp_connection_description_t
+ * @struct lsdp_connection_t
  *
  */
-struct lsdp_connection_description_t {
+struct lsdp_connection_t {
     lsdp_network_type_t nettype; /**< Type of the network.*/
     lsdp_address_type_t addrtype; /**< IP address type. */
     char *connection_address; /**< IP connection address, in dot standard 
@@ -113,16 +113,16 @@ struct lsdp_attribute_t {
 };
 
 /**
- * @typedef lsdp_media_description_t
+ * @typedef lsdp_media_t
  *
  */
-typedef struct lsdp_media_description_t lsdp_media_description_t;
+typedef struct lsdp_media_t lsdp_media_t;
 
 /**
- * @struct lsdp_media_description_t
+ * @struct lsdp_media_t
  *
  */
-struct lsdp_media_description_t {
+struct lsdp_media_t {
     lsdp_media_type_t media;
     int port;
     char *proto;
@@ -130,16 +130,16 @@ struct lsdp_media_description_t {
 };
 
 /**
- * @typedef lsdp_description_t
+ * @typedef lsdp_session_description_t
  *
  */
-typedef struct lsdp_description_t lsdp_description_t;
+typedef struct lsdp_session_t lsdp_session_t;
 
 /**
- * @struct lsdp_description_t
+ * @struct lsdp_session_description_t
  * 
  */
-struct lsdp_description_t {
+struct lsdp_session_t {
     int v; /**< Version number, currently 0. */
     lsdp_origin_t *o;
     char *s; /**< Session name, MUST at least be one character (whitespace
@@ -148,13 +148,13 @@ struct lsdp_description_t {
     char *u; /**< URI of the description. */
     char **e; /**< Emails with optional contact names. */
     char **p; /**< Phone numbers with optional contact names. */
-    lsdp_connection_description_t *c; /**< Connection information. */
+    lsdp_connection_t *c; /**< Connection information. */
     char *b; /**< Bandwidth information. */
-    lsdp_time_description_t **times;
+    lsdp_time_t **times;
     lsdp_time_zone_t **z;
     lsdp_encryption_key_t *k;
     lsdp_attribute_t **a;
-    lsdp_media_description_t **m;
+    lsdp_media_t **m;
 
 };
 
@@ -163,13 +163,13 @@ struct lsdp_description_t {
  *
  * @return A pointer to a neutral description.
  */
-lsdp_description_t* lsdp_description_new(void);
+lsdp_session_t* lsdp_session_new(void);
 
 /**
  * @brief Free the allocated memory of every struct involved in the description.
  *
  * @param[in] desc A pointer to the description to free.
  */
-void lsdp_description_free(lsdp_description_t *desc);
+void lsdp_session_free(lsdp_session_t *sess);
 
 #endif
