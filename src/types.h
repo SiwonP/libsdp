dc32bd76a2a7a956b3b61225ff9f0a523a14027c
Simon Petit <nomisp96@hotmail.fr>
2018-06-12 17:36:42 +0200

    MINOR adding documentation

--- src/types.h
+++ src/types.h
@@ -20,12 +20,11 @@ typedef enum lsdp_network_type_t {
 } lsdp_network_type_t;
 
 /**
- * @enum lsdp_network_type_t
+ * @enum lsdp_address_type_t
  *
- * @typedef lsdp_network_type_t
+ * @typedef lsdp_address_type_t
  *
- * @brief Version of the IP used. Currently choice between IPv4 (IP4) and IPv6
- * (IP6)
+ * @brief Version of the IP used. 
  */
 typedef enum lsdp_address_type_t {
     IP4, /**< IPv4. */
@@ -36,6 +35,8 @@ typedef enum lsdp_address_type_t {
  * @enum lsdp_bandwidth_type_t
  *
  * @typedef lsdp_bandwidth_type_t
+ *
+ * @brief Type of the bandwith.
  */
 typedef enum lsdp_bandwidth_type_t {
     CT, /**< Conference Total. */
@@ -46,18 +47,23 @@ typedef enum lsdp_bandwidth_type_t {
  * @enum lsdp_encryption_method_t
  *
  * @typedef lsdp_encryption_method_t
+ *
+ * @brief Encryption method of the key
  */
 typedef enum lsdp_encryption_method_t {
-    CLEAR,
-    BASE64,
-    URI,
-    PROMPT
+    CLEAR, /**< The key is not given as is. */
+    BASE64, /**< The key has been base64 encoded. */
+    URI, /**< The uri refering to the key. */
+    PROMPT /**< No key included. */
 } lsdp_encryption_method_t;
 
 
 /**
  * @enum lsdp_media_type_t
  *
+ * @typedef lsdp_media_type_t
+ *
+ * @brief Support of the media.
  */
 typedef enum lsdp_media_type_t {
     AUDIO,
@@ -72,17 +78,26 @@ typedef enum lsdp_media_type_t {
  * @enum lsdp_attribute_type_t
  *
  * @typedef lsdp_attribute_type_t
+ *
+ * @brief Type for attributes.
  */
 typedef enum lsdp_attribute_type_t {
-    CAT,
-    KEYWDS,
-    TOOL,
-    PTIME,
-    MAXPTIME,
+    CAT, /**< */
+    KEYWDS, /**< To assist identifying wanted sessions at the receiver.
+              The receiver can chose interesting sessions based on keywords
+              describing the session's purpose. */
+    TOOL, /**< Gives the name and version of the tool used to create the 
+            session description.*/
+    PTIME, /**< Gives the length of time represented by the media in a packet. */
+    MAXPTIME, /**< Gives the maximum amount of media that can be encapsulated 
+                in each packet. */
     RTPMAP,
-    RECVONLY,
-    SENDRECV,
-    SENDONLY,
+    RECVONLY, /**< Specifies that the tools should be started in receive-only
+                mode. */
+    SENDRECV, /**< Specifies that the tools should be started in send and reveive
+                mode. */
+    SENDONLY, /**< Specifies that the tools should be started in send-only 
+                mode. */
     INACTIVE,
     ORIENT,
     TYPE,
@@ -100,13 +115,12 @@ typedef enum lsdp_attribute_type_t {
  *
  * @typedef lsdp_protocol_type_t
  *
- * @brief Protocol of the media. Currently udp (UDP), real-time protocol
- * (RTP_AVP) and secure real time protocol (RTP_SAVP) supported.
+ * @brief Protocol of the media. 
  */
 typedef enum lsdp_protocol_type_t {
-    UDP,
-    RTP_AVP,
-    RTP_SAVP
+    UDP, /**< Unspecified protocol over udp. */
+    RTP_AVP, /**< Real Time Protcol  with minimal control. */
+    RTP_SAVP /**< Secure Real Time Protocol. */
 } lsdp_protocol_type_t;
 
 #endif
